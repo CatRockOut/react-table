e1e6d7bd3fc7606fb64bde0aa031fd04499f4b7b
moshenskyi.y <moshenskyi.y@fsolutionstop_luckypal.net>
2024-12-10 17:58:35 +0200

    temp

--- tables/src/App.tsx
+++ tables/src/App.tsx
@@ -1,13 +1,50 @@
-import {Header, Body} from "./components";
 import './App.css'
+import {useEffect, useState} from "react";
+
+type DataUser = {
+    id: number;
+    name: string;
+    username: string;
+}
 
 function App() {
+    const [data, setData] = useState<DataUser[]>();
+
+    useEffect(() => {
+        const API = 'https://jsonplaceholder.typicode.com/users';
+
+        (async () => {
+            try {
+                const response = await fetch(API);
+                const json = await response.json();
+
+                setData(json);
+            } catch (error) {
+                console.log(error);
+            }
+        })();
+    }, []);
+
     return (
         <table>
-            <Header/>
-            <Body/>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>NAME</th>
+                    <th>USERNAME</th>
+                </tr>
+            </thead>
+            <tbody>
+                {data && data.map((item) => (
+                    <tr key={item.id}>
+                        <td>{item.id}</td>
+                        <td>{item.name}</td>
+                        <td>{item.username}</td>
+                    </tr>
+                ))}
+            </tbody>
         </table>
-    )
+    );
 }
 
 export default App
