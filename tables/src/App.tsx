dbd1718c7b492054122693032ffcc9e1a97177f6
moshenskyi.y <moshenskyi.y@fsolutionstop_luckypal.net>
2024-12-23 17:25:00 +0200

    temp

--- tables/src/App.tsx
+++ tables/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import {useEffect, useState} from "react";
+import arrow from './assets/arrow.svg'
 
 type DataUser = {
     id: number;
@@ -7,8 +8,19 @@ type DataUser = {
     username: string;
 }
 
+type SortOrder = {
+    id: null | 'arrowUpFlag' | 'arrowDownFlag';
+    name: null | 'arrowUpFlag' | 'arrowDownFlag';
+    username: null | 'arrowUpFlag' | 'arrowDownFlag';
+};
+
 function App() {
     const [data, setData] = useState<DataUser[]>([]);
+    const [sortOrder, setSortOrder] = useState<SortOrder>({
+        id: null,
+        name: null,
+        username: null,
+    });
 
     useEffect(() => {
         const API = 'https://jsonplaceholder.typicode.com/users';
@@ -25,30 +37,72 @@ function App() {
         })();
     }, []);
 
-    const handleColumnSort = () => {
-        const newSortData = data.sort((a, b) => a.id - b.id);
+    const handleIdSort = () => {
+        if (sortOrder.id === null) {
+            setSortOrder((prevState) => ({...prevState, id: 'arrowUpFlag'}));
 
-        console.log('newSortData: ', newSortData);
-        setData([...newSortData]);
+            setData([...data].sort((a, b) => b.id - a.id));
+        } else if (sortOrder.id === 'arrowUpFlag') {
+            setSortOrder((prevState) => ({...prevState, id: 'arrowDownFlag'}));
+
+            setData([...data].sort((a, b) => a.id - b.id));
+        } else {
+            setSortOrder((prevState) => ({...prevState, id: null}));
+
+            setData([...data]);
+        }
     };
+
+    const handleStringSort = (key: keyof DataUser) => {
+        const sortedString = [...data].sort((a, b) => {
+            if (typeof a[key] === 'string' && typeof b[key] === 'string') {
+                return a[key].localeCompare(b[key]);
+            }
+
+            return a[key] > b[key] ? 1 : -1;
+        });
+
+        setData(sortedString);
+    };
+
+    const getArrowClass = (order: null | 'arrowUpFlag' | 'arrowDownFlag') => {
+        if (order === 'arrowUpFlag') {
+            return 'arrow-up';
+        }
+
+        if (order === 'arrowDownFlag') {
+            return 'arrow-down';
+        }
+
+        return '';
+    }
 
     return (
         <table>
             <thead>
-                <tr>
-                    <th onClick={handleColumnSort}>ID</th>
-                    <th>NAME</th>
-                    <th>USERNAME</th>
-                </tr>
+            <tr>
+                <th onClick={handleIdSort}>
+                    ID
+                    <img src={arrow} alt="arrow" className={getArrowClass(sortOrder.id)}/>
+                </th>
+                <th onClick={() => handleStringSort('name')}>
+                    NAME
+                    <img src={arrow} alt="arrow" className={getArrowClass(sortOrder.name)}/>
+                </th>
+                <th onClick={() => handleStringSort('username')}>
+                    USERNAME
+                    <img src={arrow} alt="arrow" className={getArrowClass(sortOrder.username)}/>
+                </th>
+            </tr>
             </thead>
             <tbody>
-                {data && data.map((item) => (
-                    <tr key={item.id}>
-                        <td>{item.id}</td>
-                        <td>{item.name}</td>
-                        <td>{item.username}</td>
-                    </tr>
-                ))}
+            {data && data.map((item) => (
+                <tr key={item.id}>
+                    <td>{item.id}</td>
+                    <td>{item.name}</td>
+                    <td>{item.username}</td>
+                </tr>
+            ))}
             </tbody>
         </table>
     );
